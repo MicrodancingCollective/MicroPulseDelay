81a54f7eaf6977f4fd0c175e5feaf1c0fecb9382
MicrodancingCollective <microdancingcollective@gmail.com>
2024-12-09 19:52:44 +0100

    s

---
+++ Sourses/MicroPulseDelay.h
@@ -0,0 +1,18 @@
+//
+//  Delay_MicroPulse.h
+//  Delay MicroPulse
+//
+//  Created by Emiliano Varela on 9/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Delay_MicroPulse.
+FOUNDATION_EXPORT double Delay_MicroPulseVersionNumber;
+
+//! Project version string for Delay_MicroPulse.
+FOUNDATION_EXPORT const unsigned char Delay_MicroPulseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Delay_MicroPulse/PublicHeader.h>
+
+
